74a8460e7c5fd7d18bcdc90d9d44d62ba55fc0e2
marconimp <marconimp@yahoo.com.br>
2020-01-17 16:47:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 sudo apt-get update && apt-get upgrade -y && apt-get install htop -y
 sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
-sudo apt-get install psmisc
 sudo apt-get install unzip
-sudo apt-get install unrar
 
 wget https://raw.githubusercontent.com/marconimp/4.5.18-offline/master/flussonic-erlang_18.0.1_amd64.deb
 wget https://raw.githubusercontent.com/marconimp/4.5.18-offline/master/flussonic-python_2.7.3_amd64.deb
 wget https://raw.githubusercontent.com/marconimp/4.5.18-offline/master/flussonic-ffmpeg_4.1_amd64.deb
 wget https://raw.githubusercontent.com/marconimp/4.5.18-offline/master/flussonic_4.5.18_amd64.deb
+
 wget https://raw.githubusercontent.com/marconimp/4.5.18-offline/master/hack.zip
 
 unzip hack.zip
 
-dpkg -i flussonic-erlang_18.0.1_amd64.deb
-dpkg -i flussonic-python_2.7.3_amd64.deb
-dpkg -i flussonic-ffmpeg_4.1_amd64.deb
-dpkg -i flussonic_4.5.18_amd64.deb
+dpkg -i flussonic-python_18.11_amd64.deb
+dpkg -i flussonic-ffmpeg-base_4.6_amd64.deb
+dpkg -i flussonic-ffmpeg_4.6_amd64.deb
+dpkg -i flussonic-erlang_21.1.4_amd64.deb
+dpkg -i flussonic_18.12.1_amd64.deb
 
 # H@C|<
 
