12d66c8dcc1bcb686bd04f4f22c4b875ed2d72f5
marconimp <marconimp@yahoo.com.br>
2020-01-17 17:43:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,11 +20,4 @@ dpkg -i flussonic-ffmpeg_4.6_amd64.deb
 dpkg -i flussonic-erlang_21.1.4_amd64.deb
 dpkg -i flussonic_18.12.1_amd64.deb
 
-# H@C|<
-
-cp -r hack/siphoned/* /opt/flussonic/apps/online/ebin/
-cp hack/online/online.app /opt/flussonic/apps/online/ebin/online.app
-cp hack/online/license4.beam /opt/flussonic/apps/online/ebin/license4.beam
-cp hack/online/license_agent.beam /opt/flussonic/apps/online/ebin/license_agent.beam
-
 echo "flu starts with the following command:  /etc/init.d/flussonic run"
